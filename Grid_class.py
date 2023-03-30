27369de2251844757e8038b2fd0565fc8764bb2e
ashishpatidar123 <ashishp21@iitk.ac.in>
2023-03-30 22:57:07 +0530

    First Commit

---
+++ Grid_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Grid(gym.Env):
+    def _init_(self):
+        pass
+    
+    def spawn_canteens(self):
+        pass
+
---
+++ pacmen_class.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+class Pacmen(gym.Env):
+    def __init__(self):
+        pass
+        
+    def move(self):
+        pass
+        
+    def reproduce(self, action):
+        pass
+        
+    def consume_ghost_tax(self):
+        pass
+    
+    def share(self):
+        pass
+    
+    def refill(self):
+        pass
+    
+    def die(self):
+        pass
+    
+    def history(self):
+        pass
+
